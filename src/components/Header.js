03ac708fd1d024b5455065b17763db26fb0b2ebc
marleigh <volcana@gmail.com>
2021-11-26 11:37:20 -0800

    css for the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 export default function Header() {
     return (
-        <header>
-            header
+        <header className="header">
+            <h3 className="header--headline">MemeGenerator</h3>
+            <p className="header--subhead">React Course - Project 3</p>
         </header>
     )
 }
