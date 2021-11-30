586aac85cb645ed85701d8967e44a1d9a5612ad4
marleigh <volcana@gmail.com>
2021-11-30 11:55:05 -0800

    installing on mac. adding challenge comments

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,24 @@
 import memes from '../memeData';
 
+  /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
 export default function Meme(){
+
+    const [url, setUrl] = React.useState('');
 
     function handleClick() {
         const memeArray = memes.data.memes; //makes this less disgusting
         const randomNum = Math.floor((Math.random() * memeArray.length) + 1);
-        const {url} = memeArray[randomNum]; //makes it able to pull the url property from this array object automatically?? magic
-        console.log(url)
+        // memeArray[randomNum]; 
     }
 
     return (
